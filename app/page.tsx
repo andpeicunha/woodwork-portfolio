80390333f6956bc8e37a6da0cb8f82c95d971e04
Peixoto <andre.peixoto@trin.ca>
2023-11-16 17:53:47 -0300

    fix npm package

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
 			<Button
-				buttonColor='warning'
 				buttonSize='sm'
+				buttonColor='warning'
 			>
 				Teste
 			</Button>
+			<button className='bg-red-500 rounded-lg px-6 py-3'>Teste</button>
+			<div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'></div>
 		</main>
 	)
 }
