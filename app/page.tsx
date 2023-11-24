ef3c1e57c5b7048c1a7f8fbee4b81d1d0977d19f
Peixoto <andre.peixoto@trin.ca>
2023-11-24 11:09:21 -0300

    resolving conflict

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
 			<Button
+<<<<<<< Updated upstream
 				buttonSize='sm'
 				buttonColor='warning'
 			>
@@ -11,6 +12,13 @@ export default function Home() {
 			</Button>
 			<button className='bg-red-500 rounded-lg px-6 py-3'>Teste</button>
 			<div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'></div>
+=======
+				buttonColor='warning'
+				buttonSize='sm'
+			>
+				Teste
+			</Button>
+>>>>>>> Stashed changes
 		</main>
 	)
 }
